1edec5623ddb610aa8a387d0514bf5855cdedc58
Minami Itsukaichi <mitukaichi@gmail.com>
2023-10-24 21:01:04 -0700

    add delete function

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const path = require('path');
 const fs = require('fs');
 const generateUniqueId = require('generate-unique-id');
-const noteData = require("./db/db.json");
+let noteData = require("./db/db.json");
 const PORT = 3000; //placeholder until this is deployed in Heroku
 
 // Sets up the Express App
@@ -38,8 +38,7 @@ app.get('*', (req, res) => {
   });
 
 
-// `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. 
-//You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+// `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 
 app.post("/api/notes", function(req, res) {
     // req.body hosts is equal to the JSON post sent from the user
@@ -52,9 +51,25 @@ app.post("/api/notes", function(req, res) {
       err ? console.error(err) : console.log(noteData));
     // append the added task to the `db.json` file,
     res.json(noteData);
-  });
+});
 
 //`DELETE /api/notes/:id` should receive a query parameter that contains the id of a note to delete. To delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+
+app.delete("/api/notes/:id", function(req, res) {
+    let deleteId = req.params.id;
+    console.log("deleteId: "+ deleteId);
+    for (let i = 0; i < noteData.length; i++){
+        console.log("noteData[i].id "+ noteData[i].id);
+        if (noteData[i].id === deleteId) {
+            noteData.splice(i,1);
+            console.log(noteData);
+            break;
+        };
+    }
+    fs.writeFile('./db/db.json',JSON.stringify(noteData),(err) =>
+      err ? console.error(err) : console.log(noteData));
+    res.json(noteData);
+});
 
 // Listen
 // =============================================================
